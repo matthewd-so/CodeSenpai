dd001098fbe8448c6df094749e5cbc383f821652
Aiturgan <aiturgan888@gmail.com>
2024-09-14 20:02:38 -0400

    frontend landing

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -13,6 +13,7 @@ import { render } from "react-dom";
 enum Stage {
     Intorduction,
     Motivation,
+    SignUp,
     Promise,
 }
 
@@ -25,9 +26,7 @@ const LandingPage = ({
 }) => {
     const [currentStage, setCurrentStage] = useState<Stage>(Stage.Intorduction);
     const [username, setUsername] = useState<string>("");
-    const [verified, setVerified] = useState<boolean>(false);
-
-    const [verifiedCertain, setVerifiedCertain] = useState<boolean>(false);
+    const [goal, setGoal] = useState<string>("");
 
     const renderContent = () => {
         switch (currentStage) {
@@ -50,7 +49,7 @@ const LandingPage = ({
                             className="bg-[#B3A1CF] font-suse text-black py-[10px] px-[40px] font-semibold rounded-[30px] border border-black"
                             onClick={() => setCurrentStage(Stage.Motivation)}
                         >
-                            Let's meet!
+                            Let's talk!
                         </button>
                     </>
                 );
@@ -60,25 +59,50 @@ const LandingPage = ({
                         <img
                             src={girlSmiling}
                             alt="Your anime wifu smiling"
-                            className="h-[400px]"
+                            className="h-[350px]"
                         />
                         <div
-                            className="bg-[#B3A1CF]/50 border-4 text-2xl  border-[#E3A6D1] py-6 mb-0 font-suse text-violet-900 text-bold
-                       px-6 "
+                            className="bg-[#B3A1CF]/50 border-4 text-2xl  border-[#E3A6D1] py-6 mb-0 font-suse text-violet-900 text-bold 
+                        px-6 "
                         >
                             <p>
                                 Hi, I’m Lia, but you can call me Code-Senpai! I
                                 want to be a Software Engineer and I love
                                 problem solving, but sometimes it get's lonely..
-                                <br></br>You look cute! Tell me about yourself!
+                                You look cute! Tell me about yourself!
                             </p>
                         </div>
-                        <p className="text-xl mb-4">
-                            Let's start our coding journey together.
-                        </p>
+                        <div className="w-9/12 ml-[20%]">
+                            <p className="text-violet-900 font-suse font-semibold text-lg">
+                                {" "}
+                                My name is
+                                <input
+                                    className="inline appearance-none border-2 w-40 py-2 px-3 mx-4 placeholder:text-text_2  bg-[#B3A1CF]/50 rounded leading-tight focus:outline-none focus:border-violet-600"
+                                    type="text"
+                                    placeholder="Eshaan"
+                                    value={username}
+                                    onChange={(e) =>
+                                        setUsername(e.target.value)
+                                    }
+                                    required={true}
+                                />
+                                and I want to learn more about
+                                <input
+                                    className="inline appearance-none border-2 w-40 py-2 px-3 mx-4 placeholder:text-text_2  bg-[#B3A1CF]/50 rounded leading-tight focus:outline-none focus:border-violet-600"
+                                    type="text"
+                                    placeholder="Hashmaps"
+                                    value={goal}
+                                    onChange={(e) =>
+                                        setUsername(e.target.value)
+                                    }
+                                    required={true}
+                                />
+                                !
+                            </p>
+                        </div>
 
                         <button
-                            className="bg-[#B3A1CF] font-suse text-purple py-[10px] px-[40px] rounded-[30px] border border-black"
+                            className="bg-[#B3A1CF]/50 font-suse font-semibold text-violet-700 text-purple py-[10px] px-[40px] rounded-[30px] border-4 border-[#E3A6D1]"
                             onClick={() => setCurrentStage(Stage.Promise)}
                         >
                             Next Step
@@ -110,49 +134,8 @@ const LandingPage = ({
         }
     };
 
-    useEffect(() => {
-        if (!id) {
-            setVerified(false);
-            setVerifiedCertain(true);
-        }
-        axios
-            .get(`${API_URL}/api/accounts/id/${id}`, {
-                headers: {
-                    Authorization: token,
-                },
-            })
-            .then(({ data }) => {
-                setUsername(data.username);
-                setVerified(true);
-                setVerifiedCertain(true);
-            })
-            .catch((e: AxiosError) => {
-                setVerified(false);
-                setVerifiedCertain(true);
-            });
-    }, []);
     return (
         <div className="w-[100vw] h-[100vh] overflow-hidden absolute bg-white">
-            {verifiedCertain && verified ? (
-                <MainHeading
-                    data={{
-                        username: username,
-                        status: "loggedin",
-                    }}
-                />
-            ) : verifiedCertain === true && verified === false ? (
-                <MainHeading
-                    data={{
-                        status: "not-loggedin",
-                    }}
-                />
-            ) : (
-                <MainHeading
-                    data={{
-                        status: "none",
-                    }}
-                />
-            )}
             <div className="">
                 <div className="circle-1-animation absolute top-[6%] left-[55%] -translate-x-1/2 w-[500px] h-[500px] rounded-full filter blur-[99px] bg-gradient-to-br from-transparent to-indigo-500 opacity-60 z-10"></div>
                 <div className="absolute circle-2-animation top-[8%] left-[45%] -translate-x-1/2 w-[500px] h-[500px] rounded-full filter blur-[99px] bg-gradient-to-br from-transparent to-pink-500 opacity-60 z-10"></div>
@@ -163,41 +146,41 @@ const LandingPage = ({
                 <div className="absolute circle-7-animation top-[10%] left-[50%] -translate-x-1/2 w-[400px] h-[400px] rounded-full filter blur-[99px] bg-gradient-to-br from-transparent to-orange-800 opacity-60 z-10"></div>
             </div>
             {/* {verifiedCertain && verified ? (
-               <>
-                   <h1 className="absolute text-[38px] md:text-[48px] mx-auto text-center font-bold mt-[100px] z-50 inset-0 top-[100px]">
-                       <TypeAnimation
-                           sequence={[
-                               `Welcome back ${username}!`,
-                               2000,
-                               `Ready for more challenges, ${username}?`,
-                               2000,
-                               "Let's dive in!",
-                           ]}
-                           wrapper="span"
-                           cursor={true}
-                           style={{
-                               fontSize: "1em",
-                               display: "inline-block",
-                           }}
-                       />
-                   </h1>
-                   <p className="absolute md:w-1/2 w-3/4 text-center mx-auto mt-[50px] z-50 inset-0 md:top-[300px] top-[400px]">
-                       Ready to conquer complex challenges? Explore our Problem
-                       List now!
-                   </p>
-                   <div className="absolute md:top-[450px] top-[550px] left-1/2 -translate-x-1/2 z-50">
-                       <Link
-                           to="/problemset"
-                           className="relative ml-[8px] font-bold inline-block bg-gradient-to-r from-orange-500 to-red-600 rounded-md text-black text-[18px] hover:bg-red-800"
-                       >
-                           <div className="w-full h-full bg-black text-white py-[6px] px-[16px] rounded-[6px] border border-black hover:bg-[#00000000] hover:border-[#00000000] hover:text-black transition active:bg-red-700">
-                               Problem List
-                           </div>
-                       </Link>
-                   </div>
-               </>
-           ) : verifiedCertain === true && verified === false ? (
-               <> */}
+                <>
+                    <h1 className="absolute text-[38px] md:text-[48px] mx-auto text-center font-bold mt-[100px] z-50 inset-0 top-[100px]">
+                        <TypeAnimation
+                            sequence={[
+                                `Welcome back ${username}!`,
+                                2000,
+                                `Ready for more challenges, ${username}?`,
+                                2000,
+                                "Let's dive in!",
+                            ]}
+                            wrapper="span"
+                            cursor={true}
+                            style={{
+                                fontSize: "1em",
+                                display: "inline-block",
+                            }}
+                        />
+                    </h1>
+                    <p className="absolute md:w-1/2 w-3/4 text-center mx-auto mt-[50px] z-50 inset-0 md:top-[300px] top-[400px]">
+                        Ready to conquer complex challenges? Explore our Problem
+                        List now!
+                    </p>
+                    <div className="absolute md:top-[450px] top-[550px] left-1/2 -translate-x-1/2 z-50">
+                        <Link
+                            to="/problemset"
+                            className="relative ml-[8px] font-bold inline-block bg-gradient-to-r from-orange-500 to-red-600 rounded-md text-black text-[18px] hover:bg-red-800"
+                        >
+                            <div className="w-full h-full bg-black text-white py-[6px] px-[16px] rounded-[6px] border border-black hover:bg-[#00000000] hover:border-[#00000000] hover:text-black transition active:bg-red-700">
+                                Problem List
+                            </div>
+                        </Link>
+                    </div>
+                </>
+            ) : verifiedCertain === true && verified === false ? (
+                <> */}
 
             <div className="absolute flex flex-col items-center justify-center space-y-6   z-50 inset-0 mx-auto">
                 {renderContent()}
